fe357d606b1e5a23f2cd2c11d0612a416fd5ddca
Training <Training@192.168.252.215>
2020-03-11 12:55:50 +0530

    //project1 glass class

---
+++ project1/src/project1/Glass.java
@@ -0,0 +1,12 @@
+package project1;
+
+public class Glass {
+	public static void main(String agrs[])
+	{
+		System.out.println("glass1");
+		System.out.println("glass2");
+		System.out.println("glass3");
+		System.out.println("glass4");
+	}
+
+}

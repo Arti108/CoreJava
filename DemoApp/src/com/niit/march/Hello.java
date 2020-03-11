cbac75619d6be3ed328fd2a9d14948cc95f00811
Training <Training@192.168.252.215>
2020-03-11 12:37:25 +0530

    //initiated by Arti

---
+++ DemoApp/src/com/niit/march/Hello.java
@@ -0,0 +1,13 @@
+package com.niit.march;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("hiiiiiiiiiiiiiiiiiiii");
+		System.out.println("hiiiiiiiiiiiiiiiiiiii");
+		System.out.println("hiiiiiiiiiiiiiiiiiiii");
+		System.out.println("hiiiiiiiiiiiiiiiiiiii");
+		System.out.println("hiiiiiiiiiiiiiiiiiiii");
+	}
+
+}
